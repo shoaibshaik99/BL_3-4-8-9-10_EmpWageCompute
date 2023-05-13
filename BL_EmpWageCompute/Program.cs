5e128ca89150740435dc10fa14d8b7059306227a
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-13 23:16:00 +0530

    [Shoaib] Syncing full time employee daily wage calculation feature

--- BL_EmpWageCompute/Program.cs
+++ BL_EmpWageCompute/Program.cs
@@ -5,17 +5,23 @@
         static void Main(string[] args)
         {
             const int IS_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Portal");
             Random random = new Random();
             int randomInput = random.Next(0, 2);//0 or 1
             if (IS_PRESENT == randomInput)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is Present");
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Wage per day:{0}", empWage);
 
         }
     }
