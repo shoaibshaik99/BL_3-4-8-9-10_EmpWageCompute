2046ac21b144216847a4016f2f9792d9d01cb217
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-13 22:56:37 +0530

    [Shoaib] Syncing employee attendance feature

--- BL_EmpWageCompute/Program.cs
+++ BL_EmpWageCompute/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 1;
             Console.WriteLine("Welcome to Employee Wage Computation Portal");
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);//0 or 1
+            if (IS_PRESENT == randomInput)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
         }
     }
 }
