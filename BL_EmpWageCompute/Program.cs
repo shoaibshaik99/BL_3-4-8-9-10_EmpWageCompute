3ebb0f602024867d3c40c46401c57fb2c57fabb7
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-14 00:04:56 +0530

    [Shoaib] Adding the constrained wage feature, previously incomplete code had been pushed

--- BL_EmpWageCompute/Program.cs
+++ BL_EmpWageCompute/Program.cs
@@ -8,12 +8,15 @@
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
+            int day = 1;
+            int totalHrs = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Portal");
             Random random = new Random();
-            for (int day = 1; day <= MAX_WORKING_DAYS; day++)
+            while (day <= MAX_WORKING_DAYS && totalHrs < MAX_WORKING_HRS)
             {
                 int randomInput = random.Next(0, 3);//0 or 1 or 2
                 switch (randomInput)
@@ -28,16 +31,17 @@
                         //Console.WriteLine("Part time employee is present");
 
                         break;
-                    default: //absent
-                        empHrs = 0;
+                        //default: //absent
                         //Console.WriteLine("Employee is absent");
-                        break;
+                        //break;
                 }
                 empWage = EMP_RATE_PER_HR * empHrs;
-                totalWage += empWage; //totalwage = totalWage + empWage;
+                totalWage += empWage; //totalWage =totalWage+empWage;
+                totalHrs += empHrs;
                 Console.WriteLine("Employee Wage per day{0}:{1}", day, empWage);
+                day++;
             }
-            Console.WriteLine("Total wage for {0} days is {1}", MAX_WORKING_DAYS, totalWage);
+            Console.WriteLine("Total Wage for {0} days and Hrs:{1} is {2}", (day - 1), totalHrs, totalWage);
         }
     }
 }
