b58efaa7f12939582c3256b1c065b74207eb8a00
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-13 22:52:57 +0530

    [Shoaib] first sync of local and remote repositories

---
+++ BL_EmpWageCompute/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BL_EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Portal");
+        }
+    }
+}
