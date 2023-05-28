50173071719f7232aef16e19fddbcceae58041e4
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-28 23:33:30 +0530

    [Shoaib] Refactoring the code to use OOPs concept, previously UC8 was pushed out

--- BL_EmpWageCompute/Program.cs
+++ BL_EmpWageCompute/Program.cs
@@ -4,26 +4,29 @@ namespace BL_EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Portal");
             CalcEmpWage CalculateWage = new CalcEmpWage();
-            CalculateWage.CalcWage("TataSteel", 30, 20, 140);
-            CalculateWage.CalcWage("Sony", 40, 22, 160);
+            CalculateWage.CalcWage();
         }
 
         internal class CalcEmpWage
         {
-            public const int FULL_TIME = 1;
-            public const int PART_TIME = 2;
+            const int EMP_RATE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
 
-            public void CalcWage(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+            int empHrs = 0;
+            int empWage = 0;
+            int totalWage = 0;
+            int day = 1;
+            int totalHrs = 0;
+
+            public void CalcWage()
             {
-                int empHrs = 0;
-                int totalWage = 0;
-                int day = 1;
-                int totalHrs = 0;
-                //Console.WriteLine("Welcome to Employee Wage Computation Portal");
+                Console.WriteLine("Welcome to Employee Wage Computation Portal");
                 Random random = new Random();
-                while (day <= noOfWorkingDays && totalHrs < maxHrsPerMonth)
+                while (day <= MAX_WORKING_DAYS && totalHrs < MAX_WORKING_HRS)
                 {
                     int randomInput = random.Next(0, 3);//0 or 1 or 2
                     switch (randomInput)
@@ -42,14 +45,13 @@ namespace BL_EmployeeWageComputation
                             //Console.WriteLine("Employee is absent");
                             //break;
                     }
-                    int empWage = empRatePerHour * empHrs;
+                    empWage = EMP_RATE_PER_HR * empHrs;
                     totalWage += empWage; //totalWage =totalWage+empWage;
                     totalHrs += empHrs;
-                    //Console.WriteLine("Employee Wage on day{0} for working {1}hrs is {2}", day, empHrs, empWage);
+                    Console.WriteLine("Employee Wage for day{0}:{1}", day, empWage);
                     day++;
                 }
-                //Console.WriteLine(company + ": total Wage for {0} days or {1} Hrs (Employee is allowed to work until eiter of max working days or max working hours is reached first) is {2}", (day - 1), totalHrs, totalWage);
-                Console.WriteLine("Compamy: {0}, Total Wage: {1}", company, totalWage);
+                Console.WriteLine("Total Wage for {0} days or {1} Hrs (Employee is allowed to work until eiter of max working days or max working hours is reached first) is {2}", (day - 1), totalHrs, totalWage);
             }
         }
     }
