4237f34a83ab01543980c660ac5a136e9b6b7673
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-29 00:13:16 +0530

    [Shoaib] Now wage values are saved in the company objects created

--- BL_EmpWageCompute/Program.cs
+++ BL_EmpWageCompute/Program.cs
@@ -5,52 +5,69 @@ namespace BL_EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Portal");
-            CalcEmpWage CalculateWage = new CalcEmpWage();
-            CalculateWage.CalcWage("TataSteel", 30, 20, 140);
-            CalculateWage.CalcWage("Sony", 40, 22, 160);
+
+            CalcEmpWage TataSteel = new CalcEmpWage("TataSteel", 30, 20, 140);
+            TataSteel.CalcWage();
+
+            CalcEmpWage Sony = new CalcEmpWage("Sony", 40, 22, 160);
+            Sony.CalcWage();
+
+            CalcEmpWage ITC = new CalcEmpWage("ITC", 33, 26, 170);
+            ITC.CalcWage();
+        }
+    }
+
+    internal class CalcEmpWage
+    {
+        private string company;
+        private int empRatePerHour, noOfWorkingDays, maxHrsPerMonth;
+
+        public CalcEmpWage(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.noOfWorkingDays = noOfWorkingDays;
+            this.maxHrsPerMonth = maxHrsPerMonth;
         }
 
-        internal class CalcEmpWage
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+
+        public /*static*/void CalcWage(/*CalcEmpWage obj*/)
         {
-            public const int FULL_TIME = 1;
-            public const int PART_TIME = 2;
-
-            public void CalcWage(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+            int empHrs = 0;
+            int totalWage = 0;
+            int day = 1;
+            int totalHrs = 0;
+            //Console.WriteLine("Welcome to Employee Wage Computation Portal");
+            Random random = new Random();
+            while (day <= noOfWorkingDays /*obj.noOfWorkigDays*/&& totalHrs < maxHrsPerMonth/*obj.maxHrsPerMonth*/)
             {
-                int empHrs = 0;
-                int totalWage = 0;
-                int day = 1;
-                int totalHrs = 0;
-                //Console.WriteLine("Welcome to Employee Wage Computation Portal");
-                Random random = new Random();
-                while (day <= noOfWorkingDays && totalHrs < maxHrsPerMonth)
+                int randomInput = random.Next(0, 3);//0 or 1 or 2
+                switch (randomInput)
                 {
-                    int randomInput = random.Next(0, 3);//0 or 1 or 2
-                    switch (randomInput)
-                    {
-                        case FULL_TIME:
-                            empHrs = 8;
-                            //Console.WriteLine("Full time employee is present");
+                    case FULL_TIME:
+                        empHrs = 8;
+                        //Console.WriteLine("Full time employee is present");
 
-                            break;
-                        case PART_TIME:
-                            empHrs = 4;
-                            //Console.WriteLine("Part time employee is present");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        //Console.WriteLine("Part time employee is present");
 
-                            break;
-                            //default: //absent
-                            //Console.WriteLine("Employee is absent");
-                            //break;
-                    }
-                    int empWage = empRatePerHour * empHrs;
-                    totalWage += empWage; //totalWage =totalWage+empWage;
-                    totalHrs += empHrs;
-                    //Console.WriteLine("Employee Wage on day{0} for working {1}hrs is {2}", day, empHrs, empWage);
-                    day++;
+                        break;
+                        //default: //absent
+                        //Console.WriteLine("Employee is absent");
+                        //break;
                 }
-                //Console.WriteLine(company + ": total Wage for {0} days or {1} Hrs (Employee is allowed to work until eiter of max working days or max working hours is reached first) is {2}", (day - 1), totalHrs, totalWage);
-                Console.WriteLine("Compamy: {0}, Total Wage: {1}", company, totalWage);
+                int empWage = /*obj.empRatePerHour */empRatePerHour * empHrs;
+                totalWage += empWage; //totalWage =totalWage+empWage;
+                totalHrs += empHrs;
+                //Console.WriteLine("Employee Wage on day{0} for working {1}hrs is {2}", day, empHrs, empWage);
+                day++;
             }
+            //Console.WriteLine(company + ": total Wage for {0} days or {1} Hrs (Employee is allowed to work until eiter of max working days or max working hours is reached first) is {2}", (day - 1), totalHrs, totalWage);
+            Console.WriteLine("Compamy: {0}, Total Wage: {1}", /*obj.company*/company, totalWage);
         }
     }
 }
