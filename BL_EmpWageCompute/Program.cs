7bb9e7298c7b5cbc6d6c5d37faee92acf60c8ca5
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-31 02:57:38 +0530

    [Shoaib] Previously a class was placed within a class accidentally, corrected it now

--- BL_EmpWageCompute/Program.cs
+++ BL_EmpWageCompute/Program.cs
@@ -9,48 +9,48 @@ namespace BL_EmployeeWageComputation
             CalculateWage.CalcWage("TataSteel", 30, 20, 140);
             CalculateWage.CalcWage("Sony", 40, 22, 160);
         }
+    }
 
-        internal class CalcEmpWage
+    internal class CalcEmpWage
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+
+        public void CalcWage(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
         {
-            public const int FULL_TIME = 1;
-            public const int PART_TIME = 2;
-
-            public void CalcWage(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+            int empHrs = 0;
+            int totalWage = 0;
+            int day = 1;
+            int totalHrs = 0;
+            //Console.WriteLine("Welcome to Employee Wage Computation Portal");
+            Random random = new Random();
+            while (day <= noOfWorkingDays && totalHrs < maxHrsPerMonth)
             {
-                int empHrs = 0;
-                int totalWage = 0;
-                int day = 1;
-                int totalHrs = 0;
-                //Console.WriteLine("Welcome to Employee Wage Computation Portal");
-                Random random = new Random();
-                while (day <= noOfWorkingDays && totalHrs < maxHrsPerMonth)
+                int randomInput = random.Next(0, 3);//0 or 1 or 2
+                switch (randomInput)
                 {
-                    int randomInput = random.Next(0, 3);//0 or 1 or 2
-                    switch (randomInput)
-                    {
-                        case FULL_TIME:
-                            empHrs = 8;
-                            //Console.WriteLine("Full time employee is present");
+                    case FULL_TIME:
+                        empHrs = 8;
+                        //Console.WriteLine("Full time employee is present");
 
-                            break;
-                        case PART_TIME:
-                            empHrs = 4;
-                            //Console.WriteLine("Part time employee is present");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        //Console.WriteLine("Part time employee is present");
 
-                            break;
-                            //default: //absent
-                            //Console.WriteLine("Employee is absent");
-                            //break;
-                    }
-                    int empWage = empRatePerHour * empHrs;
-                    totalWage += empWage; //totalWage =totalWage+empWage;
-                    totalHrs += empHrs;
-                    //Console.WriteLine("Employee Wage on day{0} for working {1}hrs is {2}", day, empHrs, empWage);
-                    day++;
+                        break;
+                        //default: //absent
+                        //Console.WriteLine("Employee is absent");
+                        //break;
                 }
-                //Console.WriteLine(company + ": total Wage for {0} days or {1} Hrs (Employee is allowed to work until eiter of max working days or max working hours is reached first) is {2}", (day - 1), totalHrs, totalWage);
-                Console.WriteLine("Compamy: {0}, Total Wage: {1}", company, totalWage);
+                int empWage = empRatePerHour * empHrs;
+                totalWage += empWage; //totalWage =totalWage+empWage;
+                totalHrs += empHrs;
+                //Console.WriteLine("Employee Wage on day{0} for working {1}hrs is {2}", day, empHrs, empWage);
+                day++;
             }
+            //Console.WriteLine(company + ": total Wage for {0} days or {1} Hrs (Employee is allowed to work until eiter of max working days or max working hours is reached first) is {2}", (day - 1), totalHrs, totalWage);
+            Console.WriteLine("Compamy: {0}, Total Wage: {1}", company, totalWage);
         }
     }
 }
