bda5f99b658e2d6649a0cc14870637449ced9e94
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-04 11:54:12 +0530

    [Shoaib] Refactored the code to add CompanyManager class and improve wage calculation and storage

--- BL_EmpWageCompute/CompanyDetails.cs
+++ BL_EmpWageCompute/CompanyDetails.cs
@@ -4,6 +4,7 @@
     {
         public string company;
         public int empRatePerHour, noOfWorkingDays, maxHrsPerMonth, totalWage;
+        public List<int> dailyWages;
 
         public CompanyDetails(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
         {
@@ -11,11 +12,21 @@
             this.empRatePerHour = empRatePerHour;
             this.noOfWorkingDays = noOfWorkingDays;
             this.maxHrsPerMonth = maxHrsPerMonth;
+            this.dailyWages = new List<int>();
         }
 
         public void SetTotalEmpWage(int totalEmpWage)
         {
             this.totalWage = totalEmpWage;
+        }
+
+        public void PrintDailyWages()
+        {
+            Console.WriteLine("Daily wages for {0}:", company);
+            for (int i = 0; i < dailyWages.Count; i++)
+            {
+                Console.WriteLine("Day {0}: {1}", i + 1, dailyWages[i]);
+            }
         }
     }
 }
---
+++ BL_EmpWageCompute/CompanyManager.cs
@@ -0,0 +1,18 @@
+﻿namespace BL_EmployeeWageComputation
+{
+    internal class CompanyManager
+    {
+        public List<CompanyDetails> listOfCompanies;
+
+        public CompanyManager()
+        {
+            listOfCompanies = new List<CompanyDetails>();
+        }
+
+        public void AddCompanyDetails(string company, int empRatePerHour, int noOfWorkingDays, int maxHrsPerMonth)
+        {
+            CompanyDetails comp = new CompanyDetails(company, empRatePerHour, noOfWorkingDays, maxHrsPerMonth);
+            listOfCompanies.Add(comp);
+        }
+    }
+}
